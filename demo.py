aa0480b44ca76b308e1c518b828e2652b407b112
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2010-09-13 11:28:44 +0200

    Revert "files from central repository deleted (hoping to pull them out again!)"
    This reverts commit cb9e76171dc908e9720cd949f2bdb7c25374dbb1.

---
+++ demo.py
@@ -0,0 +1,123 @@
+#! /usr/bin/python
+""" base cherrypy launcher for the IPOL demo app """
+
+import cherrypy
+from mako.lookup import TemplateLookup
+
+def err_tb():
+    """
+    replace the default error response
+    with an cgitb HTML traceback
+    """
+    import cgitb, sys
+    tb = cgitb.html(sys.exc_info())
+    def set_tb():
+        """ set the traceback output """
+        cherrypy.response.body = tb
+        cherrypy.response.headers['Content-Length'] = None
+    cherrypy.request.hooks.attach('after_error_response', set_tb)
+
+
+class demo_index:
+    """
+    simplistic demo index used as the root app
+    """
+
+    def __init__(self, indexd=None):
+        """
+        initialize with demo_dict for indexing
+        """
+        if indexd == None:
+            self.indexd = {}
+        else:
+            self.indexd = indexd
+
+    @cherrypy.expose
+    def index(self):
+        """
+        simple demo index page
+        """
+
+        tmpl_dir = os.path.join(os.path.dirname(__file__),
+                                'base_template')
+        tmpl_lookup = TemplateLookup(directories=[tmpl_dir],
+                                     input_encoding='utf-8',
+                                     output_encoding='utf-8',
+                                     encoding_errors='replace')
+        return tmpl_lookup.get_template('index.html')\
+            .render(indexd=self.indexd,
+                    title="Demonstrations",
+                    description="foo")
+
+if __name__ == '__main__':
+
+    import os
+    import shutil
+
+    # config file and location settings
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    conf_file = os.path.join(base_dir, 'demo.conf')
+    conf_file_example = os.path.join(base_dir, 'demo.conf.example')
+
+    demo_dict = {}
+    demo_blacklist = ['.git', 'base_template']
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    cherrypy.log("app base_dir: %s" % base_dir,
+                 context='SETUP', traceback=False)
+    try:
+        cherrypy.config.update(conf_file)
+    except IOError:
+        cherrypy.log("warning: the conf file is missing, copying the example conf",
+                     context='SETUP', traceback=False)
+        shutil.copy(conf_file_example, conf_file)
+        cherrypy.config.update(conf_file)
+
+    # load the demo collection
+    # from now, the demo id is the demo module name, which happens to
+    # also be the folder name
+    for demo_id in os.listdir(base_dir):
+        if not os.path.isdir(os.path.join(base_dir, demo_id)):
+            continue
+        if demo_id in demo_blacklist:
+            continue
+        # function version of `from demo_id import app as demo.app`
+        demo = __import__(demo_id, globals(), locals(), ['app'], -1)
+        # filter test demos
+        if (cherrypy.config['server.environment'] == 'production'
+            and demo.app.is_test):
+            continue
+        cherrypy.log("loading demo: %s" % demo_id, context='SETUP',
+                     traceback=False)
+        demo_dict[demo_id] = demo.app
+        mount_point = '/' + demo_id
+        # static subfolders config
+        input_dir = os.path.join(base_dir, demo_id, 'input')
+        tmp_dir = os.path.join(base_dir, demo_id, 'tmp')
+        config = {'/input':
+                      {'tools.staticdir.on' : True,
+                       'tools.staticdir.dir' : input_dir
+                       },
+                  '/tmp':
+                      {'tools.staticdir.on' : True,
+                       'tools.staticdir.dir' : tmp_dir
+                       },
+                  }
+        # create missing static subfolders
+        if not os.path.isdir(input_dir):
+            cherrypy.log("warning: missing input folder, creating it",
+                         context='SETUP', traceback=False)
+            os.mkdir(input_dir)
+        if not os.path.isdir(tmp_dir):
+            cherrypy.log("warning: missing tmp folder, creating it",
+                         context='SETUP', traceback=False)
+            os.mkdir(tmp_dir)
+        # mount static subfolders
+        cherrypy.tree.mount(demo.app(), mount_point, config=config)
+
+    # use cgitb error handling
+    # enable via config
+    # [/]
+    # tools.cgitb.on = True
+    cherrypy.tools.cgitb = cherrypy.Tool('before_error_response', err_tb)
+    
+    cherrypy.quickstart(demo_index(demo_dict), config=conf_file)
---
+++ my_affine_sift/app.py
@@ -0,0 +1,123 @@
+"""
+ASIFT demo interaction script
+"""
+
+from base_demo import app as base_app
+from lib import get_check_key, http_redirect_303, app_expose, index_dict
+from lib import image
+from lib import TimeoutError, RuntimeError
+import os.path
+import time
+
+class app(base_app):
+    """ template demo app """
+    
+    title = "ASIFT: A New Framework for Fully Affine Invariant Comparison"
+    description = """This program performs the affine scale-invariant
+    matching method known as ASIFT.<br />
+    Please select two images; color images will be converted into gray
+    level."""
+
+    input_nb = 2
+    input_max_pixels = None
+    input_max_method = 'zoom'
+    input_dtype = '1x8i'
+    input_ext = '.png'
+    display_ext = '.png'
+    timeout = 60
+    is_test = False
+
+    def __init__(self):
+        """
+        app setup
+        """
+        # setup the parent class
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        base_app.__init__(self, base_dir)
+
+        # select the base_app steps to expose
+        # index() is generic
+        app_expose(base_app.index)
+        app_expose(base_app.input_select)
+        app_expose(base_app.input_upload)
+        # params() is modified from the template
+        app_expose(base_app.params)
+        # run() and result() must be defined here
+
+    # run() is defined here,
+    # because the parameters validation depends on the algorithm
+    @get_check_key
+    def run(self):
+        """
+        params handling and run redirection
+        """
+        # no parameters
+        http_redirect_303(self.url('result', {'key':self.key}))
+        urld = {'input' : [self.url('tmp', 'input_0.png'),
+                           self.url('tmp', 'input_1.png')]}
+        return self.tmpl_out("run.html", urld=urld)
+    run.exposed = True
+
+    # run_algo() is defined here,
+    # because it is the actual algorithm execution, hence specific
+    # run_algo() is called from result(),
+    # with the parameters validated in run()
+    def run_algo(self, stdout=None, timeout=False):
+        """
+        the core algo runner
+        could also be called by a batch processor
+        this one needs no parameter
+        """
+        asift = self.run_proc(['asift', 'input_0.png', 'input_1.png', 
+                               'outputV.png', 'outputH.png',
+                               'match.txt', 'keys_0.txt', 'keys_1.txt'],
+                              stdout=stdout, stderr=stdout)
+        sift = self.run_proc(['sift', 'input_0.png', 'input_1.png', 
+                              'outputV_SIFT.png', 'outputH_SIFT.png',
+                              'match_SIFT.txt',
+                              'keys_0_SIFT.txt', 'keys_1_SIFT.txt'],
+                             stdout=None, stderr=None)
+        self.wait_proc([asift, sift], timeout)
+        return
+
+    @get_check_key
+    def result(self):
+        """
+        display the algo results
+        SHOULD be defined in the derived classes, to check the parameters
+        """
+        # no parameters
+        stdout = open(self.path('tmp', 'stdout.txt'), 'w')
+        try:
+            run_time = time.time()
+            self.run_algo(timeout=self.timeout, stdout=stdout)
+            run_time = time.time() - run_time
+        except TimeoutError:
+            return self.error(errcode='timeout',
+                              errmsg="""
+The algorithm took more than %i seconds and had to be interrupted.
+Try again with simpler images.""" % self.timeout)
+        except RuntimeError:
+            return self.error(errcode='retime',
+                              errmsg="""
+The program ended with a failure return code,
+something must have gone wrong""")
+        self.log("input processed")
+
+        urld = {'new_run' : self.url('params'),
+                'new_input' : self.url('index'),
+                'input' : [self.url('tmp', 'input_0.png'),
+                           self.url('tmp', 'input_1.png')],
+                'output_h' : self.url('tmp', 'outputH.png'),
+                'output_v' : self.url('tmp', 'outputV.png'),
+                'output_v_sift' : self.url('tmp', 'outputV_SIFT.png'),
+                'match' : self.url('tmp', 'match.txt'),
+                'keys_0' : self.url('tmp', 'keys_0.txt'),
+                'keys_1' : self.url('tmp', 'keys_1.txt')}
+        stdout = open(self.path('tmp', 'stdout.txt'), 'r')
+        img_out = image(self.path('tmp', 'outputV.png'))
+        return self.tmpl_out("result.html", urld=urld,
+                             run_time="%0.2f" % run_time,
+                             height=str(img_out.size[1]+10),
+                             stdout=stdout.read())
+    result.exposed = True
