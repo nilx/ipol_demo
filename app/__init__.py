4e5de0c3fad4ae34b3b89a239dca7ec20c949224
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2010-10-19 13:52:32 +0900

    build the demo_dict via list-comprehension

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,11 @@ import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-def get_demo_dict():
-    """
-    build a demo dictionary
+# (id:app) demo dict
+demo_dict = dict([(demo_id,
+                   # function version of `from demo_id import app as _.app`
+                   __import__(demo_id, globals(), locals(), ['app'], -1).app)
+                  for demo_id in os.listdir(base_dir)
+                  if os.path.isdir(os.path.join(base_dir, demo_id))])
+                  
 
-    @return (id:app) demo dict
-    """
-    demo_dict = {}
-    for demo_id in os.listdir(base_dir):
-        if not os.path.isdir(os.path.join(base_dir, demo_id)):
-            continue
-        # function version of `from demo_id import app as _tmp.app`
-        _tmp = __import__(demo_id, globals(), locals(), ['app'], -1)        
-        demo_app = _tmp.app
-        demo_dict[demo_id] = demo_app
-    return demo_dict
-
-demo_dict = get_demo_dict()
