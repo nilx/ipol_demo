3f3c8c316bfd3b000fa031bab1d8b45deacc9771
José Luis <joseluis@ubuntu.(none)>
2011-04-13 12:23:46 +0200

    test: removed app/denoise_functions/

--- app/denoise_functions/addnoise_function.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ADD_NOISE_FUNCTION_H_
-#define _ADD_NOISE_FUNCTION_H_
-
-
-/* addnoise_function.cpp */
-#include <cmath>
-
-void fiAddNoise(float *u, float *v, float std, long int randinit, int size);
-
