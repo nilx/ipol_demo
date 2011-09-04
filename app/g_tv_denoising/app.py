672d1c651c987cff654224de2dada515a8e28c32
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-09-04 11:43:00 +0900

    [app/g_tv] remove pylint comments
    A project-wide rcfile can be used instead.

--- app/g_tv_denoising/app.py
+++ app/g_tv_denoising/app.py
@@ -2,7 +2,6 @@
 Rudin-Osher-Fatemi Total Variation Denoising using Split-Bregman 
 ipol demo web app
 """
-# pylint: disable-msg=R0904,C0103
 
 from lib import base_app, build, http, image
 from lib.misc import ctime
