e2fcd7c362f825e598deead9607868f72634af61
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-12-22 14:36:00 +0900

    [app/bcm_non_local_means_denoising] remove useless self.cfg.save()

--- app/bcm_non_local_means_denoising/app.py
+++ app/bcm_non_local_means_denoising/app.py
@@ -151,7 +151,6 @@ class app(base_app):
                 #save parameter
                 try:
                     self.cfg['param'] = {'sigma' : sigma}
-                    self.cfg.save()
                 except ValueError:
                     return self.error(errcode='badparams',
                                       errmsg="Incorrect standard"
@@ -164,7 +163,6 @@ class app(base_app):
                                          'y0' : int(y0),
                                          'x1' : int(x),
                                          'y1' : int(y)}
-                    self.cfg.save()
                 except ValueError:
                     return self.error(errcode='badparams',
                                       errmsg="Incorrect parameters.")
@@ -209,7 +207,6 @@ class app(base_app):
                                          'y0' : y0,
                                          'x1' : x1,
                                          'y1' : y1}
-                    self.cfg.save()
                 except ValueError:
                     return self.error(errcode='badparams',
                                       errmsg="Incorrect parameters.")
@@ -242,7 +239,6 @@ class app(base_app):
             run_time = time.time()
             self.run_algo(sigma, stdout=stdout, timeout=self.timeout)
             self.cfg['info']['run_time'] = time.time() - run_time
-            self.cfg.save()
         except TimeoutError:
             return self.error(errcode='timeout') 
         except RuntimeError:
