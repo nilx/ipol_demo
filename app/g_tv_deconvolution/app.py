cd77c5c7fce9304a59a765318656f48baad1871d
Pascal Getreuer <getreuer@gmail.com>
2012-06-07 19:31:06 -0400

    [g_tv_deconvolution] src updated

--- app/g_tv_deconvolution/app.py
+++ app/g_tv_deconvolution/app.py
@@ -63,7 +63,7 @@ class app(base_app):
         """
         
         # store common file path in variables
-        archive = 'tvdeconv_20120516'
+        archive = 'tvdeconv_20120607'
         tgz_url = 'http://www.ipol.im/pub/algo/' \
             + 'g_tv_deconvolution/' + archive + '.tar.gz'
         tgz_file = self.dl_dir + archive + '.tar.gz'
