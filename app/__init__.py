0c56a8b78770ef4f9160e9d059ad8b91c068e4fb
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2013-04-26 13:50:27 +0900

    [app] resist to broken demo apps
    use exceptions to ignore faulty apps
    detect and report import errors, syntax errors and other unspecified errors

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,20 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # (id:app) demo dict
-demo_dict = dict([(demo_id,
-                   # function version of `from demo_id import app as _.app`
-                   __import__(demo_id, globals(), locals(), ['app'], -1).app)
-                  for demo_id in os.listdir(base_dir)
-                  if os.path.isdir(os.path.join(base_dir, demo_id))])
-                  
-
+demo_dict = {}
+for demo_id in os.listdir(base_dir):
+    if os.path.isdir(os.path.join(base_dir, demo_id)):
+        try:
+            # use function version of `from demo_id import app as _.app`
+            demo_dict[demo_id] = __import__(demo_id, globals(), locals(),
+                                            ['app'], -1).app
+        except ImportError:
+            print "ERROR: Import error in app '%s'" % demo_id
+            print "ERROR: Probably a missing module."
+            print "ERROR: App not loaded."
+        except SyntaxError:
+            print "ERROR: Syntax error in app '%s'" % demo_id
+            print "ERROR: App not loaded."
+        except StandardError:
+            print "ERROR: Unspecified error in app '%s'" % demo_id
+            print "ERROR: App not loaded."
