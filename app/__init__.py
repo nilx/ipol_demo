858467016ad3b42ec5b8af2a2087b56267cdf46d
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2013-04-26 13:58:52 +0900

    [app] improve app loading error messages

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,12 @@ for demo_id in os.listdir(base_dir):
             # use function version of `from demo_id import app as _.app`
             demo_dict[demo_id] = __import__(demo_id, globals(), locals(),
                                             ['app'], -1).app
-        except ImportError:
-            print "ERROR: Import error in app '%s'" % demo_id
-            print "ERROR: Probably a missing module."
+        except ImportError, e:
+            print "ERROR: Import error in app %s: %s" % (demo_id, e)
             print "ERROR: App not loaded."
-        except SyntaxError:
-            print "ERROR: Syntax error in app '%s'" % demo_id
+        except SyntaxError, e:
+            print "ERROR: Syntax error in app %s: %s" % (demo_id, e)
             print "ERROR: App not loaded."
-        except StandardError:
-            print "ERROR: Unspecified error in app '%s'" % demo_id
+        except StandardError, e:
+            print "ERROR: Unspecified error in app %s: %s" % (demo_id, e)
             print "ERROR: App not loaded."
