97846da2b9088c25d664567fa2a6ca2ef0aed9b0
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2013-05-15 12:12:25 +0900

    [utils] new init script

--- utils/init_script.sh
+++ utils/init_script.sh
@@ -19,7 +19,8 @@ BASEDIR=/srv/ipol
 SCRIPT=${BASEDIR}/demo/demo.py
 SCRIPTNAME=${SCRIPT##*/}
 SUID=www-data
-PIDFILE=${BASEDIR}/demo.pid
+PIDFILE_RUN=/var/run/ipol_demo_run.pid
+PIDFILE_BUILD=/var/run/ipol_demo_build.pid
 
 test -x $SCRIPT || exit 0
 
@@ -28,59 +29,82 @@ test -x $SCRIPT || exit 0
 export CCACHE_DIR=/tmp/.ccache
 
 case "$1" in
-    start)
-	log_daemon_msg "Starting IPOL demo" "${SCRIPT}"
-	/sbin/start-stop-daemon --start --oknodo \
-	    --user ${SUID} --name ${SCRIPTNAME} \
-	    --pidfile ${PIDFILE} --make-pidfile \
+    build)
+	# (re)build command-line binaries
+	log_daemon_msg "(Re)building IPOL demos (in background)"
+	log_progress_msg "${SCRIPT} build ... "
+	# start the process in the background as www-data
+	# save the PID
+	# fail (exit 1) if it is still running from a previous invocation
+	# we expect the process to die by itself in a reasonable time
+	/sbin/start-stop-daemon --start \
 	    --startas ${SCRIPT} \
-	    --chuid ${SUID} --background -- build run
+	    --pidfile ${PIDFILE_BUILD} --make-pidfile \
+	    --user ${SUID} --chuid ${SUID} \
+	    --background -- build
+	log_end_msg 0
+    ;;
+
+    start)
+	# start the web service
+	log_daemon_msg "Starting IPOL demo"
+	log_progress_msg "${SCRIPT} run ... "
+	# start the process in the background as www-data
+	# save the PID
+	# do nothing if it is already running
+	/sbin/start-stop-daemon --start --oknodo \
+	    --startas ${SCRIPT} \
+	    --pidfile ${PIDFILE_RUN} --make-pidfile \
+	    --user ${SUID} --chuid ${SUID} \
+	    --background -- run
 	sleep 1
-	PID=$(cat ${PIDFILE})
-	if [ -f /proc/$PID/stat ]; then
-	    # the demo server is running
+	PID=$(cat ${PIDFILE_RUN})
+	if [ -f /proc/$PID/stat ]; then # is running
 	    log_end_msg 0
-	else
-	    # the demo server failed to start
+            $0 build # (re)build demos in background
+	else                            # failed to start, error
 	    log_end_msg 1
-	    rm ${PIDFILE}
+	    rm ${PIDFILE_RUN}
 	    exit 1
 	fi
     ;;
 
     stop)
-	log_daemon_msg "Stopping IPOL demo" "${SCRIPT}"
+	# stop the web service
+	log_daemon_msg "Stopping IPOL demo"
+	if [ ! -f ${PIDFILE_RUN} ]; then # no pidfile
+	    log_end_msg 0
+	    log_daemon_cont_msg "PID file not found"
+	    log_end_msg 0
+	    exit 1
+	fi
+	PID=$(cat ${PIDFILE_RUN})
+	log_progress_msg "PID ${PID} ... "
 	/sbin/start-stop-daemon --stop --oknodo \
-	    --user ${SUID} --name ${SCRIPTNAME} --pidfile ${PIDFILE} \
+	    --pidfile ${PIDFILE_RUN} \
+	    --user ${SUID} \
 	    --retry 5
-	if [ ! -f ${PIDFILE} ]; then
+	if [ -f /proc/$PID/stat ]; then # failed to stop, error
+	    log_end_msg 1
+	    exit 1
+	else                            # is stopped
 	    log_end_msg 0
-	    log_daemon_msg "PID file not found"
-	    log_end_msg 0
-	    exit 0
-	else
-	    PID=$(cat ${PIDFILE})
-	    if [ -f /proc/$PID/stat ]; then
-		log_end_msg 1
-		exit 1
-	    else
-		log_end_msg 0
-		rm ${PIDFILE}
-	    fi
+	    rm ${PIDFILE_RUN}
 	fi
     ;;
 
     status)
-	if [ -f ${PIDFILE} ]; then
-	    PID=$(cat ${PIDFILE})
+	# check if it is running
+	if [ -f ${PIDFILE_RUN} ]; then
+	    PID=$(cat ${PIDFILE_RUN})
 	    if [ -f /proc/$PID/stat ]; then
 		echo "running"
 	    else
 		echo "not running"
-		rm ${PIDFILE}
+		rm ${PIDFILE_RUN}
 	    fi
 	else
-	    echo "not running"
+	    echo "probably not running (no PID file)"
 	fi
     ;;
 
@@ -90,7 +114,7 @@ case "$1" in
     ;;
 
     *)
-	log_action_msg "Usage: $0 {start|stop|restart|status}"
+	log_action_msg "Usage: $0 {start|stop|restart|status|build}"
 	log_end_msg 1
 	exit 1
     ;;
