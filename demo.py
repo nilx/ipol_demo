db0517201906c4eae8bb4d8373186796c27ad436
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2013-05-17 22:58:34 +0900

    simplify demo
    remove clean and scrub actions

--- demo.py
+++ demo.py
@@ -70,36 +70,6 @@ def do_build(demo_dict):
                          traceback=False)
     return
 
-def do_clean(demo_dict):
-    """
-    cleanup all the temporary folders
-    """
-    for (demo_id, demo_app) in demo_dict.items():
-        # delete temporary folders
-        demo = demo_app()
-        cherrypy.log("cleaning", context='SETUP/%s' % demo_id,
-                     traceback=False)
-        for clean_dir in [demo.tmp_dir, demo.bin_dir,
-                          demo.dl_dir, demo.src_dir]:
-            if os.path.isdir(clean_dir):
-                shutil.rmtree(clean_dir)
-    return
-
-def do_scrub(demo_dict):
-    """
-    cleanup all the temporary and archive folders
-    """
-    for (demo_id, demo_app) in demo_dict.items():
-        demo = demo_app()
-        cherrypy.log("scrubbing", context='SETUP/%s' % demo_id,
-                     traceback=False)
-        for scrub_dir in [demo.tmp_dir, demo.bin_dir,
-                          demo.dl_dir, demo.src_dir,
-                          demo.archive_dir]:
-            if os.path.isdir(scrub_dir):
-                shutil.rmtree(scrub_dir)
-    return
-
 def do_run(demo_dict):
     """
     run the demo app server
@@ -165,38 +135,19 @@ if __name__ == '__main__':
                 demo_dict.pop(demo_id)
 
     # now handle the remaining command-line options
-
-    # default behaviour : run
+    # default action is "run"
     if len(sys.argv) == 1:
         sys.argv += ["run"]
-
     for arg in sys.argv[1:]:
         if "build" == arg:
             do_build(demo_dict)
         elif "run" == arg:
             do_run(demo_dict)
-        elif "clean" == arg:
-            do_clean(demo_dict)
-        elif "scrub" == arg:
-            ans = raw_input("\nDo you really want to delete the archives? ")
-            if ans in ("y", "Y", "yes", "YES", "1"):
-                do_scrub(demo_dict)
-            else:
-                print "Glad I asked! Nothing deleted."
         else:
             print """
 usage: %(argv0)s [action]
 
 actions:
+* run     launch the web service (default)
 * build   build/update the compiled programs
-* run     launch the web service
-* clean   delete all temporary files
-* scrub   delete all temporary and archive files
-
-notes:
-* Default action is "run".
-* Multiple successive actions can be specified. They will be processed
-  in the arguments order. Note that the script will hang when the server
-  is run.
-* "scrub" requires interactive confirmation.
 """ % {'argv0' : sys.argv[0]}
