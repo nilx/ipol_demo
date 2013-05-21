71357019e56c2e9e96dd9e21e7bc545752d159e7
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2013-05-22 00:58:35 +0900

    [app/45] source URL

--- app/45/app.py
+++ app/45/app.py
@@ -56,7 +56,7 @@ class app(base_app):
         program build/update
         """
         version = 4
-        zip_filename = 'ponomarenko_v%d.zip' % ((version))
+        zip_filename = 'source.zip'
         src_dir_name = 'ponomarenko_v%d' % ((version))
         prog_filename = 'ponomarenko'
         # store common file path in variables
