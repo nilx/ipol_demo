2f6b5be7b37eeffb18ddf86b276fcda39066045e
Miguel Colom <miguel.pixel@gmail.com>
2013-12-24 20:07:13 +0100

    [app/108] Set is_test = False

--- app/108/app.py
+++ app/108/app.py
@@ -22,7 +22,7 @@ class app(base_app):
     input_max_weight = 10 * 1024 * 1024 # max size (in bytes) of an input file
     input_dtype = '3x8i' # input image expected data type
     input_ext = '.png' # input image expected extension (ie file format)
-    is_test = True
+    is_test = False
 
     def __init__(self):
         """
