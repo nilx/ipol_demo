995b293061b710b7cce0126733d0f0b29f8a1ef7
Miguel Colom <miguel.pixel@gmail.com>
2014-04-05 17:57:43 +0200

    [app/90] source URL update

--- app/90/app.py
+++ app/90/app.py
@@ -64,7 +64,7 @@ class app(base_app):
         prog_file = self.bin_dir + prog_filename
         log_file = self.base_dir + "build.log"
         # get the latest source archive
-        build.download('http://www.ipol.im/pub/pre/90/' + \
+        build.download('http://www.ipol.im/pub/art/2013/90/' + \
                        zip_filename, tgz_file)
 
         # test if the dest file is missing, or too old
